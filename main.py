a442ec305b0924b0c361472d6f78abe07183eef4
jonas <jonas@stirnemann.xyz>
2025-04-21 02:41:43 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def add(
+    a: int,
+    b: int,
+) -> int:
+    return a + b
+
+
+def subtract(
+    a: int,
+    b: int,
+) -> int:
+    return a - b
+
+
+def main():
+    print("This is a simple template!")
+
+
+if __name__ == "__main__":
+    main()
