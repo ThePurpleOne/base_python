d12e3d889d78ce508db01c9275e3955634c535ea
Jonas STIRNEMANN <ThePurpleOne@users.noreply.github.com>
2025-05-03 16:33:08 +0200

    2 substract (#4)
    * "fix: changed addition.py file into operations.py"
    
    Since it will containe the file will contain other operations than the
    addition
    
    * feat[operations.py]: added the subtract operation

--- src/addition.py
+++
@@ -1,8 +0,0 @@
-def add(
-    a: int,
-    b: int,
-) -> int:
-    """
-    Simple demo type hinting function
-    """
-    return a + b
---
+++ src/operations.py
@@ -0,0 +1,18 @@
+def add(
+    a: int,
+    b: int,
+) -> int:
+    """
+    Simple demo type hinting function
+    """
+    return a + b
+
+
+def subtract(
+    a: int,
+    b: int,
+) -> int:
+    """
+    Simple demo type hinting function
+    """
+    return a - b
